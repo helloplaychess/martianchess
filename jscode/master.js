2d6354f1202c151c46d676a26792ebeea67a40cc
helloplaychess <98226151+helloplaychess@users.noreply.github.com>
2022-01-30 06:44:42 +0100

    Add files via upload
    basic visual features are done. non-functioning board placed in the center already.

--- jscode/master.js
+++ jscode/master.js
@@ -1,10 +1,23 @@
 function setup() {
-  var canvas = createCanvas(540, 1080);
+  var canvas = createCanvas(600, 1200);
   canvas.parent("super");
 }
 
 function draw() {
-  background(127);
+  background(191, 191, 191)
+  for (let i = 0; i < 8; i++){
+    for (let j = 0; j < 4; j++){
+      noStroke();
+      fill(63, 63, 63);
+      rect(j*150, i*150, 150, 150)
+      fill(127, 127, 127);
+      rect(j*150+25, i*150+25, 100, 100)
+      //console.log(i*150, j*150, i*150+150, j*150+150);
+      //replace this with images
+    }
+  }
+
+
 }
 
 console.log("1: master.js has been loaded")
