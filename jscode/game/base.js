29661c666a99de005a7431ce33655e0d2f952b63
helloplaychess <98226151+helloplaychess@users.noreply.github.com>
2022-02-06 08:14:58 +0100

    added movement for drones and score changes

--- jscode/game/base.js
+++ jscode/game/base.js
@@ -45,6 +45,11 @@ var gameState = {
                 if(this.currentBoard[j+1][i+1] == 0){ //destination cell is empty
                   legals.push(pseudoMove);
                 }else if(this.currentBoard[j+1][i+1] != 0 && isCapture(j, j+1)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j+1][i+1]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j+1][i+1]
+                  }
                   legals.push(pseudoMove);
                 }
 
@@ -58,6 +63,11 @@ var gameState = {
                 if(this.currentBoard[j+1][i-1] == 0){ //destination cell is empty
                   legals.push(pseudoMove);
                 }else if(this.currentBoard[j+1][i-1] != 0 && isCapture(j, j+1)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j+1][i-1]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j+1][i-1]
+                  }
                   legals.push(pseudoMove);
                 }
 
@@ -71,6 +81,11 @@ var gameState = {
                 if(this.currentBoard[j-1][i+1] == 0){ //destination cell is empty
                   legals.push(pseudoMove);
                 }else if(this.currentBoard[j-1][i+1] != 0 && isCapture(j, j-1)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j-1][i+1]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j-1][i+1]
+                  }
                   legals.push(pseudoMove);
                 }
 
@@ -84,6 +99,11 @@ var gameState = {
                 if(this.currentBoard[j-1][i-1] == 0){ //destination cell is empty
                   legals.push(pseudoMove);
                 }else if(this.currentBoard[j-1][i-1] != 0 && isCapture(j, j-1)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j-1][i-1]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j-1][i-1]
+                  }
                   legals.push(pseudoMove);
                 }
 
@@ -91,6 +111,134 @@ var gameState = {
 
             }else if(this.currentBoard[j][i] == 2){ //cell has drone
 
+              pseudoMove = Object.create(move);
+              pseudoMove.location = [i, j];
+              pseudoMove.destination = [i, j-1]; //up 1
+              pseudoMove.pieceType = this.currentBoard[j][i];
+              if(j-1 >= 0 && j-1 <= 7 && i >= 0 && i <= 3){
+                if(this.currentBoard[j-1][i] == 0){ //destination cell is empty
+                  legals.push(pseudoMove);
+                }else if(this.currentBoard[j-1][i] != 0 && isCapture(j, j-1)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j-1][i]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j-1][i]
+                  }
+                  legals.push(pseudoMove);
+                }
+              }
+              pseudoMove = Object.create(move);
+              pseudoMove.location = [i, j];
+              pseudoMove.destination = [i, j-2]; //up 2
+              pseudoMove.pieceType = this.currentBoard[j][i];
+              if(j-2 >= 0 && j-2 <= 7 && i >= 0 && i <= 3){
+                if(this.currentBoard[j-1][i] == 0 && this.currentBoard[j-2][i] == 0){ //destination cell is empty
+                  legals.push(pseudoMove);
+                }else if(this.currentBoard[j-1][i] == 0 && this.currentBoard[j-2][i] != 0 && isCapture(j, j-2)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j-2][i]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j-2][i]
+                  }
+                  legals.push(pseudoMove);
+                }
+              }
+              pseudoMove = Object.create(move);
+              pseudoMove.location = [i, j];
+              pseudoMove.destination = [i, j+1]; //down 1
+              pseudoMove.pieceType = this.currentBoard[j][i];
+              if(j+1 >= 0 && j+1 <= 7 && i >= 0 && i <= 3){
+                if(this.currentBoard[j+1][i] == 0){ //destination cell is empty
+                  legals.push(pseudoMove);
+                }else if(this.currentBoard[j+1][i] != 0 && isCapture(j, j+1)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j+1][i]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j+1][i]
+                  }
+                  legals.push(pseudoMove);
+                }
+              }
+              pseudoMove = Object.create(move);
+              pseudoMove.location = [i, j];
+              pseudoMove.destination = [i, j+2]; //down 2
+              pseudoMove.pieceType = this.currentBoard[j][i];
+              if(j+2 >= 0 && j+2 <= 7 && i >= 0 && i <= 3){
+                if(this.currentBoard[j+1][i] == 0 && this.currentBoard[j+2][i] == 0){ //destination cell is empty
+                  legals.push(pseudoMove);
+                }else if(this.currentBoard[j+1][i] == 0 && this.currentBoard[j+2][i] != 0 && isCapture(j, j+2)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j+2][i]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j+2][i]
+                  }
+                  legals.push(pseudoMove);
+                }
+              }
+              pseudoMove = Object.create(move);
+              pseudoMove.location = [i, j];
+              pseudoMove.destination = [i-1, j]; //left 1
+              pseudoMove.pieceType = this.currentBoard[j][i];
+              if(j >= 0 && j <= 7 && i-1 >= 0 && i-1 <= 3){
+                if(this.currentBoard[j][i-1] == 0){ //destination cell is empty
+                  legals.push(pseudoMove);
+                }else if(this.currentBoard[j][i-1] != 0 && isCapture(j, j)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j][i-1]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j][i-1]
+                  }
+                  legals.push(pseudoMove);
+                }
+              }
+              pseudoMove = Object.create(move);
+              pseudoMove.location = [i, j];
+              pseudoMove.destination = [i-2, j]; //left 2
+              pseudoMove.pieceType = this.currentBoard[j][i];
+              if(j >= 0 && j <= 7 && i-2 >= 0 && i-2 <= 3){
+                if(this.currentBoard[j][i-1] == 0 && this.currentBoard[j][i-2] == 0){ //destination cell is empty
+                  legals.push(pseudoMove);
+                }else if(this.currentBoard[j][i-1] == 0 && this.currentBoard[j][i-2] != 0 && isCapture(j, j)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j][i-2]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j][i-2]
+                  }
+                  legals.push(pseudoMove);
+                }
+              }
+              pseudoMove = Object.create(move);
+              pseudoMove.location = [i, j];
+              pseudoMove.destination = [i+1, j]; //right 1
+              pseudoMove.pieceType = this.currentBoard[j][i];
+              if(j >= 0 && j <= 7 && i+1 >= 0 && i+1 <= 3){
+                if(this.currentBoard[j][i+1] == 0){ //destination cell is empty
+                  legals.push(pseudoMove);
+                }else if(this.currentBoard[j][i+1] != 0 && isCapture(j, j)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j][i+1]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j][i+1]
+                  }
+                  legals.push(pseudoMove);
+                }
+              }
+              pseudoMove = Object.create(move);
+              pseudoMove.location = [i, j];
+              pseudoMove.destination = [i+2, j]; //right 2
+              pseudoMove.pieceType = this.currentBoard[j][i];
+              if(j >= 0 && j <= 7 && i+2 >= 0 && i+2 <= 3){
+                if(this.currentBoard[j][i+1] == 0 && this.currentBoard[j][i+2] == 0){ //destination cell is empty
+                  legals.push(pseudoMove);
+                }else if(this.currentBoard[j][i+1] == 0 && this.currentBoard[j][i+2] != 0 && isCapture(j, j)){ //destination cell is not empty and can be captured
+                  if(this.currentTurn == 1){
+                    pseudoMove.scoreChangeBottom = this.currentBoard[j][i+2]
+                  }else if(this.currentTurn == 0){
+                    pseudoMove.scoreChangeTop = this.currentBoard[j][i+2]
+                  }
+                  legals.push(pseudoMove);
+                }
+              }
 
 
             }else if(this.currentBoard[j][i] == 3){ //cell has queen
@@ -110,8 +258,12 @@ var gameState = {
   execute: function(move){
     if(this.currentTurn == 0){
       this.currentTurn = 1;
+      this.scoreTop = this.scoreTop + move.scoreChangeTop;
+      this.scoreBottom = this.scoreBottom + move.scoreChangeBottom;
     }else if(this.currentTurn == 1){
       this.currentTurn = 0;
+      this.scoreTop = this.scoreTop + move.scoreChangeTop;
+      this.scoreBottom = this.scoreBottom + move.scoreChangeBottom;
     }
     this.currentBoard[move.location[1]][move.location[0]] = 0;
     this.currentBoard[move.destination[1]][move.destination[0]] = move.pieceType;
